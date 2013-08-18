b9d70cb238c584c94eaf5db889454fc8a3e09e95
Sergio Correia <sergio@correia.cc>
2013-08-17 21:06:25 -0400

    run git pull on submodules, so that they are updatedt

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ sudo pacman -S --needed gvim clang ctags git tmux xorg-xwininfo xorg-xprop gnome
 
 git submodule update --init
 git submodule foreach git checkout master
+git submodule foreach git pull
 
 for file in $(ls -A -I .git -I .gitignore -I .gitmodules -I README.md -I setup.sh -I .*.swp); do
     fqfile=$PWD/$file
